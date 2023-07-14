b8474a61302cae0258ca3ef1ec359325fc93015b
seanny3 <dev.seann@gmail.com>
2023-07-14 14:22:55 +0900

    0714 opencv_on_web completed

--- diary/src/opencv_on_web/app.py
+++ diary/src/opencv_on_web/app.py
@@ -20,42 +20,32 @@ cv_tf = []
 global angle
 angle = 90
 
-# init_file_names = os.listdir('./resources/video')
-# for file_name in init_file_names:
-#     os.remove('./resources/video/' + file_name)
-    
-# init_model_names = os.listdir('./resources/model')
-# for model_name in init_model_names:
-#     os.remove('./resources/model/' + model_name)
+global cam_stop
+cam_stop = False
     
 @app.route('/')
 def video_show():
     return render_template('index.html')
 
-@app.route('/upload/video', methods=['POST'])
-def upload_video():
-    file_names = os.listdir('./resources/video')
+@app.route('/init/resources', methods=['POST'])
+def init_resources():
+    global cam_stop
+    cam_stop = True
     
-    for file_name in file_names:
-        os.remove('./resources/video/' + file_name)
-    
-    file = request.files['file']
-    ext = file.filename.split(".")[-1]
-    file_path = './resources/video/uploaded.' + ext
-    file.save(file_path)
     return redirect("/")
 
-@app.route('/upload/model', methods=['POST'])
-def upload_model():
-    file_names = os.listdir('./resources/model')
+@app.route('/upload/<method>', methods=['POST'])
+def upload_video(method):
+    file_names = os.listdir(f'./resources/{method}')
     
     for file_name in file_names:
-        os.remove('./resources/model/' + file_name)
+        os.remove(f'./resources/{method}/{file_name}')
     
     file = request.files['file']
     ext = file.filename.split(".")[-1]
-    file_path = './resources/model/uploaded.' + ext
+    file_path = f'./resources/{method}/uploaded.{ext}'
     file.save(file_path)
+    
     return redirect("/")
 
 @app.route('/video')
@@ -66,9 +56,10 @@ def video():
     else:
         return Response("none")
 
+
 @app.route('/cam')
 def cam():
-    return Response(gen_frames(str(0)), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(0), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def gen_frames(file):
     global cv_noise, cv_filter, cv_edge, cv_cvt, cv_tf
@@ -79,148 +70,168 @@ def gen_frames(file):
     cv_tf = []
     global angle
     angle = 90
+    global cam_stop
+    cam_stop = False
     
     model_name = os.listdir("./resources/model/")
     if model_name:
-        # model = torch.hub.load('../../../../yolov7', 'custom',  './resources/model/uploaded.pt', source='local')
-        model = torch.hub.load('../../../../yolov5', 'custom',  '../../../../yolov5/yolov5s.pt', source='local')
-        # model = YOLO('./resources/model/uploaded.pt')
+        model = YOLO('./resources/model/uploaded.pt')
     
-    ext = file.split(".")[-1]
+    size = (640,480)
+    cap = cv2.VideoCapture(file)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, size[0])
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, size[1])
     
-    if ext in ["mp4", "avi", "0"]:
-        cap = cv2.VideoCapture(0 if ext == "0" else file)
+    while True:
+        start_time = time()
+        ret, frame = cap.read()
         
-        while True:
-            start_time = time()
+        if cam_stop:
+            cap.release()
             
-            ret, frame = cap.read()
+            init_file_names = os.listdir('./resources/video')
+            for file_name in init_file_names:
+                os.remove('./resources/video/' + file_name)
+                
+            init_model_names = os.listdir('./resources/model')
+            for model_name in init_model_names:
+                os.remove('./resources/model/' + model_name)
+                
+            cam_stop = False
             
-            if not ret:
-                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-                continue
-            
+            return redirect("/")
+        
+        if not ret:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            continue
+                
+        else:
+            h,w=frame.shape[:2]
+            ash=size[1]/h
+            asw=size[0]/w
+            if asw<ash:
+                sizeas=(int(w*asw),int(h*asw))
             else:
-                # 이미지 변환
-                if 'rotate90' in cv_tf:
-                    height, width = frame.shape[:2]
-                    rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), -angle, 1.0)
-                    output_width = int(height * (width / height))
-                    frame = cv2.warpAffine(frame, rotation_matrix, (output_width, height))
-                if 'leftright' in cv_tf:
-                    frame = cv2.flip(frame, 1)
-                if 'updown' in cv_tf:
-                    frame = cv2.flip(frame, 0)
-                
-                # 노이즈 추가 기능
-                if cv_noise == 'salt':
-                    frame = add_salt_and_pepper_noise(frame, 0.05)
-                elif cv_noise == 'gaus':
-                    frame = add_gaussian_noise(frame, 20)
-                
-                # 그레이스케일 변환
-                if cv_cvt == 'gray':
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                
-                # 필터링 기능
-                if cv_filter == 'avg':
-                    frame = cv2.blur(frame, (3,3))
-                elif cv_filter == 'med':
-                    frame = cv2.medianBlur(frame, 3)
-                elif cv_filter == 'gaus':
-                    frame = cv2.GaussianBlur(frame, (5,5), 1)
-                elif cv_filter == 'sharp1':
-                    kernel = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
-                    frame = cv2.filter2D(frame, -1, kernel)
-                elif cv_filter == 'sharp2':
-                    kernel = np.array([[1,1,1],[1,-7,1],[1,1,1]])
-                    frame = cv2.filter2D(frame, -1, kernel)
-                elif cv_filter == 'sharp3':
-                    kernel = np.array([[-1,-1,-1,-1,-1],[-1,2,2,2,-1],[-1,2,8,2,-1],[-1,2,2,2,-1],[-1,-1,-1,-1,-1]])/8.0
-                    frame = cv2.filter2D(frame, -1, kernel)
-                
-                # 윤곽선 검출 기능 - 그레이스케일 필수
-                if cv_edge:
-                    if len(frame.shape) == 3:  # 이미지가 컬러 이미지인 경우
-                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                        
-                if cv_edge == 'robert':          
-                    # 로버트 크로스 필터
-                    gx = np.array([[-1, 0], [0, 1]], dtype=int)
-                    gy = np.array([[0, -1], [1, 0]], dtype=int)
-
-                    # 로버트 크로스 컨벌루션
-                    x = cv2.filter2D(frame, -1, gx, delta=0)
-                    y = cv2.filter2D(frame, -1, gy, delta=0)
-                
-                    frame = x+y
-                    
-                elif cv_edge == 'sobel':
-                    # Sobel operator
-                    x = cv2.Sobel(frame, -1, 1, 0, ksize=3, delta=0)
-                    y = cv2.Sobel(frame, -1, 0, 1, ksize=3, delta=0)
-                    frame = x+y
-                    
-                elif cv_edge == 'prewitt':
-                    # 프르윗 필터
-                    gx = np.array([[-1,0,1],[-1,0,1],[-1,0,1]],dtype=int)
-                    gy = np.array([[1,1,1],[0,0,0],[-1,-1,-1]],dtype=int)
-
-                    # 프르윗 필터 컨벌루션
-                    x = cv2.filter2D(frame, -1, gx, delta=0)
-                    y = cv2.filter2D(frame, -1, gy, delta=0)
-                
-                    frame = x+y
-                    
-                elif cv_edge == 'canny':
-                    frame = cv2.Canny(frame,100,200)
-                
-                elif cv_edge == 'otsucanny':
-                    # 2. Global OTSU-Canny
-                    otsu_th, otsu_binary = cv2.threshold(frame, -1, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-
-                    # 임계값 수동 설정의 번거로움 극복, 불필요한 윤곽선
-                    frame = cv2.Canny(frame, otsu_th, otsu_th)
-                    
-                elif cv_edge == 'laplacian':
-                    frame = cv2.Laplacian(frame, -1)
-                
-                if model_name:
-                    # OpenCV 이미지를 PIL 이미지로 변환
-                    pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-
-                    # 객체 인식
-                    results = model(pil_image)
-                    # frame = results[0].plot()     # YOLOv8
-                    # bounding box 처리
-                    boxes = results.xyxy[0]  # (x1, y1, x2, y2) 형식의 bounding box 좌표
-                    confidences = results.xyxy[0][:, 4]  # bounding box의 신뢰도
-
-                    for box, confidence in zip(boxes, confidences):
-                        x1, y1, x2, y2 = map(int, box[:4])
-                        label = f'{results.names[int(box[5])]} {confidence:.2f}'  # 객체 클래스와 신뢰도
-
-                        # bounding box 그리기
-                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                        cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.4, (0, 255, 0), 2)               
-                
-                    end_time = time()
-                    fps = 1/np.round(end_time - start_time, 3)
-                    cv2.putText(frame, f"FPS: {fps:.3f}", (30, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
-                    
-                    
-                ret, buffer = cv2.imencode('.jpg', frame)
-                frame = buffer.tobytes()
-
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                sizeas=(int(w*ash),int(h*ash))
+            frame = cv2.resize(frame, dsize=sizeas)
             
-    elif ext in ["jpg", "png", "jpeg", "gif"]:
-        frame = cv2.imread(file)
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
+            # 이미지 변환
+            if 'rotate90' in cv_tf:
+                height, width = frame.shape[:2]
+                rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), -angle, 1.0)
+                frame = cv2.warpAffine(frame, rotation_matrix, (width, height))
+            if 'leftright' in cv_tf:
+                frame = cv2.flip(frame, 1)
+            if 'updown' in cv_tf:
+                frame = cv2.flip(frame, 0)
+            
+            # 노이즈 추가 기능
+            if cv_noise == 'salt':
+                frame = add_salt_and_pepper_noise(frame, 0.05)
+            elif cv_noise == 'gaus':
+                frame = add_gaussian_noise(frame, 20)
+            
+            # 그레이스케일 변환
+            if cv_cvt == 'gray':
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            # 필터링 기능
+            if cv_filter == 'avg':
+                frame = cv2.blur(frame, (3,3))
+            elif cv_filter == 'med':
+                frame = cv2.medianBlur(frame, 3)
+            elif cv_filter == 'gaus':
+                frame = cv2.GaussianBlur(frame, (5,5), 1)
+            elif cv_filter == 'sharp1':
+                kernel = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
+                frame = cv2.filter2D(frame, -1, kernel)
+            elif cv_filter == 'sharp2':
+                kernel = np.array([[1,1,1],[1,-7,1],[1,1,1]])
+                frame = cv2.filter2D(frame, -1, kernel)
+            elif cv_filter == 'sharp3':
+                kernel = np.array([[-1,-1,-1,-1,-1],[-1,2,2,2,-1],[-1,2,8,2,-1],[-1,2,2,2,-1],[-1,-1,-1,-1,-1]])/8.0
+                frame = cv2.filter2D(frame, -1, kernel)
+            
+            # 윤곽선 검출 기능 - 그레이스케일 필수
+            if cv_edge:
+                if len(frame.shape) == 3:  # 이미지가 컬러 이미지인 경우
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    
+            if cv_edge == 'robert':          
+                # 로버트 크로스 필터
+                gx = np.array([[-1, 0], [0, 1]], dtype=int)
+                gy = np.array([[0, -1], [1, 0]], dtype=int)
+
+                # 로버트 크로스 컨벌루션
+                x = cv2.filter2D(frame, -1, gx, delta=0)
+                y = cv2.filter2D(frame, -1, gy, delta=0)
+            
+                frame = x+y
+                
+            elif cv_edge == 'sobel':
+                # Sobel operator
+                x = cv2.Sobel(frame, -1, 1, 0, ksize=3, delta=0)
+                y = cv2.Sobel(frame, -1, 0, 1, ksize=3, delta=0)
+                frame = x+y
+                
+            elif cv_edge == 'prewitt':
+                # 프르윗 필터
+                gx = np.array([[-1,0,1],[-1,0,1],[-1,0,1]],dtype=int)
+                gy = np.array([[1,1,1],[0,0,0],[-1,-1,-1]],dtype=int)
+
+                # 프르윗 필터 컨벌루션
+                x = cv2.filter2D(frame, -1, gx, delta=0)
+                y = cv2.filter2D(frame, -1, gy, delta=0)
+            
+                frame = x+y
+                
+            elif cv_edge == 'canny':
+                frame = cv2.Canny(frame,100,200)
+            
+            elif cv_edge == 'otsucanny':
+                # 2. Global OTSU-Canny
+                otsu_th, otsu_binary = cv2.threshold(frame, -1, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+
+                # 임계값 수동 설정의 번거로움 극복, 불필요한 윤곽선
+                frame = cv2.Canny(frame, otsu_th, otsu_th)
+                
+            elif cv_edge == 'laplacian':
+                frame = cv2.Laplacian(frame, -1)
+            
+            if model_name:
+                # OpenCV 이미지를 PIL 이미지로 변환
+                pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+                # 객체 인식
+                results = model(pil_image)
+                frame = results[0].plot()     # YOLOv8              
+            
+            if len(frame.shape) < 3:
+                frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+                
+            base_frame = np.zeros((size[1],size[0],3), np.uint8)
+            base_frame[
+                int(size[1]/2-sizeas[1]/2):int(size[1]/2+sizeas[1]/2),
+                int(size[0]/2-sizeas[0]/2):int(size[0]/2+sizeas[0]/2),
+                :
+            ] = frame
+            
+            frame = base_frame
+            
+            try:
+                end_time = time()
+                fps = 1./(end_time - start_time)
+                cv2.putText(frame, f"FPS: {fps:.3f}", (15, 35), cv2.FONT_HERSHEY_SIMPLEX, .8, (0, 0, 255), 1)
+            except:
+                fps = 0.0                 
+                
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            
+            cv2.waitKey(int(1000 / 50))  # 1 frame/sec
+
         yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     
     
 @app.route('/cv', methods=['POST'])
